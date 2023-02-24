d301a715ce0d685191e8ba805583f9e6995b75ba
Roushelf <2100012985@stu.pku.edu.cn>
2023-02-24 18:55:50 +0800

    copy some common functions

--- Simulations/MassSpringSystemSimulator.cpp
+++ Simulations/MassSpringSystemSimulator.cpp
@@ -9,7 +9,9 @@ void MassSpringSystemSimulator::initUI(DrawingUtilitiesClass* DUC) {
 
 }
 void MassSpringSystemSimulator::reset() {
-
+	m_mouse.x = m_mouse.y = 0;
+	m_trackmouse.x = m_trackmouse.y = 0;
+	m_oldtrackmouse.x = m_oldtrackmouse.y = 0;
 }
 void MassSpringSystemSimulator::drawFrame(ID3D11DeviceContext* pd3dImmediateContext) {
 
@@ -24,10 +26,14 @@ void MassSpringSystemSimulator::simulateTimestep(float timeStep) {
 
 }
 void MassSpringSystemSimulator::onClick(int x, int y) {
-
+	m_trackmouse.x = x;
+	m_trackmouse.y = y;
 }
 void MassSpringSystemSimulator::onMouse(int x, int y) {
-
+	m_oldtrackmouse.x = x;
+	m_oldtrackmouse.y = y;
+	m_trackmouse.x = x;
+	m_trackmouse.y = y;
 }
 
 // Specific Functions
